54cd476ebd1f1aaec8dbbc45d20ffe79f53b6586
Daniel Kenney <danielkenney99@yahoo.com>
2022-06-20 12:28:30 -0700

    added the main features and such. Pretty tough project ngl. Added a black and random and reset button. Works great!

--- javascript.js
+++ javascript.js
@@ -1,13 +1,35 @@
-let board = document.getElementById("board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+const container = document.getElementById("board");
+const height = prompt("Please choose the size of the grid. (less than 50px)");
+const width = height;
+container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.id = "gridBlock";
-  square.style.backgroundColor = "blue";
-  board.insertAdjacentElement("beforeend", square);
-  square.addEventListener("mousedown", function onClick() {
-    square.style.backgroundColor = "red";
+for (let i = 0; i < height * width; i++) {
+  const cell = document.createElement("div");
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  cell.id = "cell";
+  cell.style.backgroundColor = "white";
+  container.appendChild(cell);
+  cell.addEventListener("mouseenter", function onClick() {
+    cell.style.backgroundColor = "black";
+  });
+
+  const reset = document.getElementById("reset");
+  reset.addEventListener("click", function clearBoard() {
+    cell.style.backgroundColor = "white";
+  });
+
+  const blackPen = document.getElementById("blackPen");
+  blackPen.addEventListener("click", function onClick() {
+    cell.addEventListener("mouseenter", function onClick() {
+      cell.style.backgroundColor = "black";
+    });
+  });
+
+  const randomColorPen = document.getElementById("randomColorPen");
+  randomColorPen.addEventListener("click", function onClick() {
+    cell.addEventListener("mouseenter", function onClick() {
+      cell.style.backgroundColor = "#" + randomColor;
+    });
   });
 }
